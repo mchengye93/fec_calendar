e5a69e0bd47bd039bf9b0cab7512515e7a009c14
Marbo Cheng Ye <marbochengye@gmail.com>
2019-05-16 11:06:26 -0700

    webpack, eslint-airbnb

---
+++ server/index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const path = require('path');
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+const distDir = path.join(__dirname, '../client/dist');
+
+app.use(express.static(distDir));
+
+app.use(bodyParser.json());
+
+app.listen(port, () => console.log('Listening on Port:', port));
